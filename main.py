19991b42a7d5ec18de11ed8e6fe9587b26073139
Assil Ksiksi <cyph0nik@gmail.com>
2012-03-11 20:39:36 +0400

    General improvements made. Opted to use 2 lists instead of a dictionary.

--- main.py
+++ main.py
@@ -3,27 +3,31 @@ import time
 from sys import argv
 
 def file(path):
-    game_console = {}
+    game = []
+    console = []
 
-    # Populate dictionary with game:console from file.
+    # Populate lists with game:console from file.
     f = open(path)
     for line in f:
         arr = line.partition(':')
-        game_console[''.join(arr[0])] = (''.join(arr[2])).rstrip('\n')
+        game.append(''.join(arr[0]).strip())
+        console.append((''.join(arr[2])).strip())
     f.close()
     
     # Start timer
     s = time.time()
     
-    output = scrape(game_console)
+    output = scrape(game, console)
     
     # Write results to file
     f = open('scores.txt', 'w')
     f.write('Metacritic Score Grabber v0.1 - Output\n')
     f.write('--------------------------------------\n\n')
+
+    count = 0
     
-    for g, c in game_console.items():
-        current = output[g]
+    for g, c in zip(game, console):
+        current = output[count]
         f.write("{0} on {1}: ".format(g, c) + '\n')
         if 'Game' not in current:
             f.write('\n- Metascore is: ' + str(current[0]) + '\n')
@@ -32,11 +36,14 @@ def file(path):
             f.write('\n\n')
         else:
             f.write('\n' + current + '\n\n')
+        count += 1
             
     f.write("Done in %f seconds." % (time.time()-s))
+    f.close()
 
 def console():
-    game_console = {}
+    game = []
+    console = []
 
     while True:
         data = raw_input("Enter game:console (-1 to stop) -> ")
@@ -44,7 +51,8 @@ def console():
             break
         if ':' in data:
             arr = data.partition(':')
-            game_console[''.join(arr[0])] = ''.join(arr[2]) # Parses game:console to dict format
+            game.append(''.join(arr[0]))
+            console.append(''.join(arr[2])) # Parses game:console to dict format
         else:
             print "Woah. Please make sure to use the ':' as a seperator. Try again."
     print ""
@@ -52,45 +60,51 @@ def console():
     # Start timer
     s = time.time()
 
-    if game_console:
-        output = scrape(game_console)
+    if game and console:
+        output = scrape(game, console)
     else:
         print "Nothing to print.\n"
 
+    count = 0
+
     # Print scores from output to screen
-    for g, c in game_console.items():
-        current = output[g]
+    for g, c in zip(game, console):
+        current = output[count]
         print "{0} on {1}: ".format(g, c),
-        if len(current) > 1:
+        if 'Game' not in current:
             print "Metascore = {0},".format(current[0]),
             print "Userscore = {0},".format(current[1]),
             print "Combined score = {0}".format(current[2])
         else:
             print current
+        count += 1
 
     print "Done in %f seconds." % (time.time()-s)
 
 def csv(path):
-    game_console = {}
+    game = []
+    console = []
 
-    # Populate dictionary with game:console from file.
+    # Populate lists with game,console from .csv
     f = open(path)
     for line in f:
         arr = line.partition(',')
-        game_console[''.join(arr[0])] = (''.join(arr[2])).rstrip()
+        game.append(''.join(arr[0]).strip())
+        console.append((''.join(arr[2])).strip())
     f.close()
-    
+
     # Start timer
     s = time.time()
-    
-    output = scrape(game_console)
+
+    output = scrape(game, console)
     
     # Write results to file
     f = open('scores.csv', 'w')
     f.write('Game,Console,Metascore,Userscore,Combined Score\r') # Table headers
+    count = 0 # Count current game
 
-    for g, c in game_console.items():
-        current = output[g]
+    for g, c in zip(game, console):
+        current = output[count]
         f.write("{0},{1},".format(g, c))
         if 'Game' not in current:
             if current[0] == 'Not available':
@@ -103,19 +117,20 @@ def csv(path):
                 f.write(str(current[1]) + ',' + str(current[2]) + '\r')
         else:
             f.write('-,-,-\r')
+        count += 1
     f.close()
             
     print "Done in %f seconds." % (time.time()-s)
 
-def scrape(game_console):
+def scrape(game, console):
     count = 0
-    total = len(game_console)
-    output = {}
+    total = len(game)
+    output = []
 
     print "Working..."
     print ""
 
-    for g, c in game_console.items():
+    for g, c in zip(game, console):
         # Gets source code of game page
         Http = httplib2.Http()
         source = Http.request("http://www.metacritic.com/game/{0}/{1}".format(c.replace(' ', '-').lower(), g.replace(' ', '-').lower()))[1]
@@ -124,13 +139,13 @@ def scrape(game_console):
         if '<span class="error_type">Page Not Found</span>' in source:
             count += 1
             print "{0} of {1} failed...".format(count, total)
-            output[g] = "Game or console is incorrect."
+            output.append("Game or console is incorrect.")
         else:
             # Finds metascore
             index = source.find('"v:average">')
             index += len('"v:average">')
             try:
-                meta_score = float(source[index] + source[index+1])
+                meta_score = int(source[index] + source[index+1])
             except ValueError:
                 meta_score = 'Not available'
 
@@ -149,7 +164,7 @@ def scrape(game_console):
                 combined_score = 'Not available'
 
             # Append results to dict
-            output[g] = (meta_score, user_score, combined_score)
+            output.append((meta_score, user_score, combined_score))
 
             # Track and print scraping progress
             count += 1
